a7ebc1ee7a09af29b4b91a8a7ea09d9be04c6331
Kasey Hernandez-Sexton <kasey.sexton5@gmail.com>
2022-08-17 15:43:32 -0400

    more design + completed picker view for opening page

--- voiceURvote/FirstLaunchViewController.swift
+++ voiceURvote/FirstLaunchViewController.swift
@@ -7,45 +7,50 @@
 
 import UIKit
 
-class FirstLaunchViewController: UIViewController {
+let states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
 
-    let defaults = UserDefaults.standard
+class FirstLaunchViewController: UIViewController {
+    
+    
+
+    @IBOutlet weak var stateTextField: UITextField!
+    
+    
+    var pickerView = UIPickerView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        if defaults.bool(forKey: "First Launch") == true {
-            print("Second+")
-            
-            performSegue(withIdentifier: "moveToHomePage", sender: nil)
-
-            
-            //run code after first launch
-            defaults.set(true, forKey: "First Launch")
-            
-        } else {
-            
-            print("First")
-            
-            //run code during first launch
-            defaults.set(true, forKey: "First Launch")
-        }
         
-       
+        pickerView.delegate = self
+        pickerView.dataSource = self
+
+        stateTextField.inputView = pickerView
         
-    }
-    
-    
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+            }
 
 }
+    
+
+
+extension FirstLaunchViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        stateTextField.text = states[row]
+        stateTextField.resignFirstResponder()
+    }
+
+}
+
+
