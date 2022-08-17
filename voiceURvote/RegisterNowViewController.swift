6ad734dcab9e2c04bb5bd4925a8b32b2c1231abc
Kasey Hernandez-Sexton <kasey.sexton5@gmail.com>
2022-08-17 16:31:51 -0400

    halfway change for vote.org embed (fingers crossed)

---
+++ voiceURvote/RegisterNowViewController.swift
@@ -0,0 +1,37 @@
+//
+//  RegisterNowViewController.swift
+//  voiceURvote
+//
+//  Created by Kasey Sexton on 8/17/22.
+//
+
+//import UIKit
+//import WebKit
+//
+//class RegisterNowViewController: UIViewController, WKUIDelegate {
+//
+//
+//
+//    var webView: WKWebView
+//
+//    override func loadView() {
+//        let webConfiguration = WKWebViewConfiguration()
+//        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+//        webView.uiDelegate = self
+//        view = webView
+//    }
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        let myURL = URL(string: "https://register.vote.org/")
+//        let myRequest = URLRequest(url: myURL!)
+//       webView.load(myRequest)
+//
+//
+//
+//    }
+//
+//
+//
+//}
---
+++ voiceURvote/UpcomingElectionsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UpcomingElectionsViewController.swift
+//  voiceURvote
+//
+//  Created by Kasey Sexton on 8/17/22.
+//
+
+import UIKit
+
+class UpcomingElectionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
